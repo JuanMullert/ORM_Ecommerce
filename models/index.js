fb1632d4c10ec3070deb629e19916e6a21da00df
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-01-22 17:21:58 -0600

    fixed a typo

--- models/index.js
+++ models/index.js
@@ -15,13 +15,13 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tags, { 
+Product.belongsToMany(Tag, { 
   through: ProductTag,
   foreignKey: 'category_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tags.belongsToMany(Product, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'category_id',
 });
